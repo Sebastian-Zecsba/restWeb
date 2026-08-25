82dab7b16dbbdf1d3444b03647276dab2b06e52c
Sebastian <zecsba2019casarias@gmail.com>
2026-08-25 04:42:13 +0000

    DIrname

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,9 @@
 import express from "express";
+import path from "node:path";
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export class Server{ 
 
@@ -10,6 +15,12 @@ export class Server{
 
         // Public Folder
         this.app.use(express.static('public'));
+
+        this.app.get('/*splat', (req, res) => {
+           const indexPath = path.join(__dirname + '../../../public/index.html')
+           res.sendFile(indexPath);
+           
+        });
 
         this.app.listen(3000, () => {
             console.log(`Server running on port 3000`)
