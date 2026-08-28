97db9fc1edc9021440096a8e2691994fcfb5f9d4
Sebastian <zecsba2019casarias@gmail.com>
2026-08-28 03:31:18 +0000

    Showing HTML

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -5,25 +5,40 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+interface Options {
+    port: number;
+    public_path?: string; 
+}
+
 export class Server{ 
 
     private app = express();
+    private readonly port: number;
+    private readonly public_path: string;
+
+    constructor(options: Options){
+
+        const { port, public_path = 'public'} = options
+
+        this.port = port;
+        this.public_path = public_path;
+    }
 
     async start(){ 
 
         // Middleware
 
         // Public Folder
-        this.app.use(express.static('public'));
+        this.app.use(express.static(this.public_path));
 
         this.app.get('/*splat', (req, res) => {
-           const indexPath = path.join(__dirname + '../../../public/index.html')
+           const indexPath = path.join(__dirname + `../../../${this.public_path}/index.html`)
            res.sendFile(indexPath);
            
         });
 
-        this.app.listen(3000, () => {
-            console.log(`Server running on port 3000`)
+        this.app.listen(this.port, () => {
+            console.log(`Server running on port ${this.port}`)
         })
     }
 
